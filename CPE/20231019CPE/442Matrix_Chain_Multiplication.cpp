a728f1b4ea4e3afb57f3fe98bffb7473474b829e
陳國翔 <chen199940@gmail.com>
2023-10-26 23:47:15 +0800

    修改

--- CPE/20231019CPE/442Matrix_Chain_Multiplication.cpp
+++ CPE/20231019CPE/442Matrix_Chain_Multiplication.cpp
@@ -1,21 +1,17 @@
 #include<iostream>
 #include<unordered_map>
 #include<stack>
-#include<set>
-#include<algorithm>
-#define w st.begin()
-#define x st.end()
-#define y st1.begin()
-#define z st1.end()
-#define inst(xx) inserter(xx,xx.begin())
+#include<vector>
 using namespace std;
+typedef pair<int,int> pii;
 int main() {
     ios_base::sync_with_stdio(false);
     int n,a,b;
     char ch;
     string s;
     cin>>n;
-    unordered_map<char,set<int>>mp;
+    unordered_map<char,pii>mp;
+    mp['(']={0,0};
     while(n--){
         cin>>ch>>a>>b;
         mp[ch]={a,b};
@@ -23,48 +19,32 @@ int main() {
     while(cin>>s){
         if(s.size()==1){
             cout<<"0\n";
-            continue;
+            break;
         }
-        s+=")";
-        stack<set<int>>sk;
-        stack<char>bf;
-        int res=0;
-        bool fg=true;
+        stack<pii>sk;
+        vector<pii>v;
+        long long res=0;
+        bool fg=1;
         for(char it:s){
-            switch(it){
-                case ')':
-                    while(bf.size()!=0&&bf.top()!='('){
-                        if(sk.size()==0){
-                            sk.push(mp[bf.top()]);
-                        }
-                        else{
-                            set<int> st=sk.top();
-                            set<int> st1=mp[bf.top()];
-                            set<int> sdSet;
-                            set_symmetric_difference(w,x,y,z,inst(sdSet));
-                            set<int>unionSet;
-                            set_union(w,x,y,z,inst(unionSet));
-                            sk.pop();
-                            if(sdSet.size()==2){
-                                int m=1;
-                                for_each(unionSet.begin(), unionSet.end(), [&m](int item) {
-                                    m*=item;
-                                });
-                                res+=m;
-                                sk.push(sdSet);
-                            }
-                            else{
-                                fg=false;
-                            }
-                        }
-                        bf.pop();
-                    }
-                    bf.pop();
+            if(it==')'){
+                v.clear();
+                while(sk.top().first!=0){
+                    v.push_back(sk.top());
+                    sk.pop();
+                }
+                sk.pop();
+                if(v[1].second==v[0].first){
+                    res+=v[1].first*v[1].second*v[0].second;
+                    sk.push({v[1].first,v[0].second});
+                }
+                else{
+                    fg^=1;
                     break;
-                default:
-                    bf.push(it);
+                }
             }
-            if(!fg)break;
+            else{
+                sk.push(mp[it]);
+            }
         }
         cout<<(fg?to_string(res):"error")<<endl;
     }
